b4703265fee1c1002fe300c4d9444aff33eda2fb
OMussa <mussa_o@icloud.com>
2023-01-09 13:45:12 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState,useEffect } from "react";
+import alanBtn from '@alan-ai/alan-sdk-web';
+
+
 
 function App() {
+ const [cart, setCart] = useState([]) //cart is state and setCart is a function that will update cart, useState is taking the argumentn ([]) which is the inital value of cart
+ const [menuItems, setMenuItems] = useState([])
+
+ useEffect(() => {
+  alanBtn({
+      key: 'a40b38879e68d02f35bd2e3925bfb34f2e956eca572e1d8b807a3e2338fdd0dc/stage',
+      onCommand: (commandData) => {
+        console.log(commandData)
+        if(commandData.command === 'getMenu'){
+          setMenuItems(commandData.data)
+        }else if(commandData.command === 'addToCart'){
+          addToCart(commandData.data)
+        }
+      }
+  });
+}, []);
+
+const addToCart = (menuItem) => { {/*takes the menuItem */}
+setCart((oldCart) => {      
+return [...oldCart, menuItem]  
+{/* sets the cart to a new array that has all the elements of the old cart using the spread operator "..." and adding the new menuItem  */}
+})
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      {menuItems.map(menuItem =>(  //mapping through menuItems
+        <li key={menuItem.name}>
+          {menuItem.name} - ${menuItem.price} - {menuItem.category}
+          <button onClick={()=> addToCart(menuItem)}>add to cart</button>   {/*calls the addToCart function and runs it onClick */}
+          </li> 
+
+  ))} 
+      <h2>Cart</h2>
+      {cart.map((cartItem) => (
+        <li key={cartItem.name} >
+       {cartItem.name} - ${cartItem.price} - {cartItem.category}
+        </li>
+      ))}
+   </div>
+  ) 
 }
 
 export default App;
